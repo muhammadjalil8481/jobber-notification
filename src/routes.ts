a16217d6c737f0c9ecd95ede87817479bfc605df
Muhammad Jalil <muhammad.jalil@haball.pk>
2025-06-13 19:01:44 +0500

    update

--- src/routes.ts
+++ src/routes.ts
@@ -1,12 +1,10 @@
 import { Request, Router, Response } from "express";
 import { StatusCodes } from "http-status-codes";
-// import { GatewayRequestVerification } from "./gateway-request-verification";
 
 const router: Router = Router();
 
 router.get(
   "/health",
-  // GatewayRequestVerification,
   (_: Request, res: Response) => {
     res.status(StatusCodes.OK).json({ status: "ok" });
   }
